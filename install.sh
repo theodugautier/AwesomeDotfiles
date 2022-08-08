a6a28b149473cbe501d4e03884b5c0f5fd54baf2
Théo Dugautier <theo.dugautier@protonmail.com>
2022-08-08 22:39:49 +0200

    delete useless mac file from this branch

--- install.sh
+++ install.sh
@@ -2,17 +2,9 @@
 
 stow --target=$HOME git
 stow --target=$HOME tmux
-stow --target=$HOME X
 stow --target=$HOME zsh
 
 # Awesome, rofi and neovim needs to be symlinked to ~/.config
 mkdir -p ~/.config/nvim
 stow -t ~/.config/nvim vim
 
-if [ "$(uname)" != "Darwin" ]; then
-  mkdir -p ~/.config/awesome
-  stow -t ~/.config/awesome awesome
-
-  mkdir -p ~/.config/rofi
-  stow -t ~/.config/rofi rofi
-fi
