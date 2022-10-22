72503b183d9d420000a14e7f4f07458ed5c2572f
Théo Dugautier <theo.dugautier@protonmail.com>
2022-10-22 10:24:28 +0200

    Change screen disposition in windows manager

--- awesome/startup/screen.lua
+++ awesome/startup/screen.lua
@@ -3,4 +3,5 @@ local awful = require('awful')
 -- Autostart script
 awful.spawn.with_shell("feh --bg-fill ~/.config/awesome/themes/background/boat.png")
 awful.spawn.with_shell("xrandr --output HDMI-0 --rotate right")
+awful.spawn.with_shell("xset r rate 300 30")
 awful.util.spawn_with_shell("xrdb ~/.Xresources")
