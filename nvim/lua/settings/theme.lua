86b09b1c53d3ec71faabfd708de817e4eaf733b4
Torpenn <ciscau@proton.me>
2025-06-13 10:35:20 +0200

    Update linux config

--- nvim/lua/settings/theme.lua
+++ nvim/lua/settings/theme.lua
@@ -1,20 +1,40 @@
 require('github-theme').setup({
-  theme_style = "dark_default",
-  function_style = "NONE",
-  sidebars = {"qf", "vista_kind", "terminal", "packer"},
-
+  colorscheme = "dark_default",
+  options = {
+    darken = {
+      enabled = true,
+      sidebars = {
+        list = {"qf", "vista_kind", "terminal", "packer"}
+      }
+    },
+    styles = {
+      comments = 'italic',
+      functions = 'NONE',
+      keywords = 'bold',
+      variables = 'NONE',
+      conditionals = 'NONE',
+      constants = 'NONE',
+      numbers = 'NONE',
+      operators = 'NONE',
+      strings = 'NONE',
+      types = 'NONE',
+    }
+  },
   -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-  colors = {hint = "orange", error = "#ff0000"},
+  palettes = {
+    all = {
+      hint = "orange",
+      error = "#ff0000"
+    }
+  },
 
-  -- Overwrite the highlight groups
-  overrides = function(c)
-    return {
-      htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "NONE"},
+  groups = {
+    all = {
       DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
       -- this will remove the highlight groups
       TSField = {},
     }
-  end
+  }
 })
 
 vim.cmd[[colorscheme github_*]]
